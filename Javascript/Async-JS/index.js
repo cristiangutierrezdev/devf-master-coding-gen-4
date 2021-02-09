7b6c4ddfe4e9f85f9fc195fae084d96788a24143
Cristian Gutierrez <cgutierrez@hugeinc.com>
2021-02-08 22:01:31 -0500

    Clase Javascript asincrono

---
+++ Javascript/Async-JS/index.js
@@ -0,0 +1,21 @@
+// console.log('1')
+// console.log('2')
+
+// setTimeout(() => {
+//   console.log('3')
+  
+// }, 2000);
+
+// console.log('4')
+
+
+function showName(name) {
+  console.log(name)
+}
+
+function activeFuntion(fun, name) {
+  return fun(name)
+}
+
+activeFuntion(showName, 'Moha')
+
