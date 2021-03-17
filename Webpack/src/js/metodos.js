b33ba70ed48a9385e37a099ba2f95e12ddeee11f
Cristian Gutierrez <cristiandisenadorvisual@gmail.com>
2021-03-16 21:58:48 -0500

    Limpiamos la nueva rama, react

--- Webpack/src/js/metodos.js
+++
@@ -1,9 +0,0 @@
-const links = document.querySelectorAll('.mi-menu__nav-link');
-const menu = document.querySelector('.mi-menu');
-
-export function changeColor() {
-  menu.classList.toggle('mi-menu--bg-green');
-  links.forEach((link)=>{
-    link.classList.toggle('mi-menu--color-white');
-  })
-}
