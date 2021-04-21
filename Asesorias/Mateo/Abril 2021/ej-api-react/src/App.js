aecd0be8bc913da45fa2924bceeb5184d4e239aa
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-04-20 22:34:01 -0500

    API traida y puesta 1/2,  install de axios y router-dom

--- Asesorias/Mateo/Abril 2021/ej-api-react/src/App.js
+++ Asesorias/Mateo/Abril 2021/ej-api-react/src/App.js
@@ -1,10 +1,47 @@
+import React, {useState, useEffect} from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
 import Card from './components/Card';
 
 function App() {
+  const [picsum, setPicsum] = useState([]);
+
+  const showInfo = () => {
+    const cardPart = picsum.map((info) => {
+      return(
+        <Card author={info.author} />
+      );
+    });
+
+    return cardPart;
+  }
+
+  useEffect(() => {
+    const getAPI = () => {
+      // axios.get('https://picsum.photos/v2/list')
+      //   .then((response) => response.json())
+      //   .then((data) => setPicsum(data))
+      //   .catch((err) => console.log(err));
+      fetch('https://picsum.photos/v2/list')
+        .then((response) => response.json())
+        .then((data) => setPicsum(data))
+        .catch((err) => console.log(err));
+    };
+
+    getAPI()
+  }, [])
+
   return (
     <div className="App">
-      <Card />
+      <BrowserRouter>
+        <Switch>
+          <Route path='/images'>
+            {showInfo}
+          </Route>
+        </Switch>
+      </BrowserRouter>
+      
     </div>
   );
 }
--- Asesorias/Mateo/Abril 2021/ej-api-react/src/components/Card.js
+++ Asesorias/Mateo/Abril 2021/ej-api-react/src/components/Card.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Card.css';
 
-function Card() {
+function Card(props) {
     return (
         <div className='mainDiv-Card'>
-            <div className='Card-title'> <p className='p-title'>Hola</p> </div>
+            <div className='Card-title'> 
+                <p className='p-title'> {props.author} </p> 
+            </div>
             <div className='Card-desc'> {} <p className='p-desc'> asd asd asdasd asd asdasd asd asdasd asd asdasd asd asdasd asd asdasd asd asdasd asd asd</p>
                 {/* <button>More Info</button> */}
             </div>
