d415a3a8c05ba4e4953c0573b3b4b875f8dce285
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-04-20 21:44:13 -0500

    estilos para Card

--- Asesorias/Mateo/Abril 2021/ej-api-react/src/App.js
+++ Asesorias/Mateo/Abril 2021/ej-api-react/src/App.js
@@ -1,10 +1,10 @@
-
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-
+      <Card />
     </div>
   );
 }
---
+++ Asesorias/Mateo/Abril 2021/ej-api-react/src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Card.css';
+
+function Card() {
+    return (
+        <div className='mainDiv-Card'>
+            <div className='Card-title'> <p className='p-title'>Hola</p> </div>
+            <div className='Card-desc'> {} <p className='p-desc'> asd asd asdasd asd asdasd asd asdasd asd asdasd asd asdasd asd asdasd asd asdasd asd asd</p>
+                {/* <button>More Info</button> */}
+            </div>
+        </div>
+    )
+};
+
+export default Card;
