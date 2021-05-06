728fed91883047c62681590dd7532c6c72aff177
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-05-05 20:31:36 -0500

    testing bootstrap to link

--- Asesorias/Mateo/Abril 2021/ej-api-react/src/components/bootstrap-ej/BootstrapEj.js
+++ Asesorias/Mateo/Abril 2021/ej-api-react/src/components/bootstrap-ej/BootstrapEj.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Jumbotron, Button } from 'reactstrap';
+// import { Jumbotron, Button } from 'reactstrap';
 
 function bootstrapEj(props) {
     return (
         <div className='boots'>
-            <Jumbotron>
+            {/* <Jumbotron>
             <h1 className="display-3">Hello, world!</h1>
             <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
             <hr className="my-2" />
@@ -12,7 +12,9 @@ function bootstrapEj(props) {
             <p className="lead">
                 <Button color="primary">Learn More</Button>
             </p>
-            </Jumbotron>
+            </Jumbotron> */}
+
+            asd
         </div>
     )
 }
