7ccaeaed5d7adbb4efef7c5990abb9cbda79e9d0
Víctor M. Reyes <victor@devf.mx>
2021-07-13 18:50:26 -0600

    feat: add dotenv

--- KNEX-mvc/index.js
+++ KNEX-mvc/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 const PORT = process.env.PORT || 3600;
 
 // Middlewares para parsear el body de una petición
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Endpoint de prueba
-// http://localhost:3600/
 app.get('/', (req, res) => res.status(200).json({ message: 'Bienvenido 😎' }));
 
 app.use('/api/v1', require('./routers/AnimalRouter'));
