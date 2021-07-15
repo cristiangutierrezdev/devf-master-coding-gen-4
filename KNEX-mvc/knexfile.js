6c9df77ffa1e80552ef5e339e1a39aa8db61f97e
Víctor M. Reyes <vm.reyesal@gmail.com>
2021-07-14 18:59:31 -0600

    chore: config enviroment variables

--- KNEX-mvc/knexfile.js
+++ KNEX-mvc/knexfile.js
@@ -2,29 +2,29 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: process.env.HOST_PG,
-      database: process.env.DATABASE_PG,
-      user: process.env.USER_PG,
-      password: process.env.PASSWORD_PG,
+      host: process.env.HOST_PG_DEV,
+      port: process.env.PORT_DEV,
+      database: process.env.DATABASE_PG_DEV,
+      user: process.env.USER_PG_DEV,
+      password: process.env.PASSWORD_PG_DEV,
     },
   },
   staging: {
     client: 'pg',
     connection: {
-      host: process.env.HOST_PG,
-      database: process.env.DATABASE_PG,
-      user: process.env.USER_PG,
-      password: process.env.PASSWORD_PG,
+      host: process.env.HOST_PG_STA,
+      database: process.env.DATABASE_PG_STA,
+      user: process.env.USER_PG_STA,
+      password: process.env.PASSWORD_PG_STA,
     },
   },
   production: {
     client: 'pg',
     connection: {
-      host: process.env.HOST_PG,
-      database: process.env.DATABASE_PG,
-      user: process.env.USER_PG,
-      password: process.env.PASSWORD_PG,
+      host: process.env.HOST_PG_PRO,
+      database: process.env.DATABASE_PG_PRO,
+      user: process.env.USER_PG_PRO,
+      password: process.env.PASSWORD_PG_PRO,
     },
   }
-
 };
